30e453ccd609768ff40421117a67d47d7d73d845
FlorentTheb <florenthebault@gmail.com>
2025-05-26 16:25:49 +0200

    Add Grid Factory

--- SyncSnake/Grid.cs
+++ SyncSnake/Grid.cs
@@ -2,5 +2,12 @@ using System;
 
 public class Grid
 {
-    
+    private int[,] Cells;
+    public Grid(int rows, int columns)
+    {
+        if (rows > 0 && columns > 0)
+            Cells = new int[rows, columns];
+        else
+            throw new ArgumentException("row and column number must be positive.");
+    }
 }
