6e41b5332c864b7659350ce84188e5a0353a2b5f
FlorentTheb <florenthebault@gmail.com>
2025-05-28 15:43:35 +0200

    Keyboard library handling

---
+++ GameTool/Keyboard.cs
@@ -0,0 +1,27 @@
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+
+namespace GameTool;
+
+public static class Keyboard
+{
+    private static readonly Dictionary<string, KeyboardKey> KeyMap = new()
+    {
+        { "Z", KeyboardKey.W },
+        { "Q", KeyboardKey.A },
+        { "S", KeyboardKey.S },
+        { "D", KeyboardKey.D },
+    };
+    public static bool IsKeyPressed(string keyString)
+    {
+        if (!KeyMap.TryGetValue(keyString, out KeyboardKey key))
+            throw new ArgumentException($"Key {keyString} not found.");
+        return Raylib.IsKeyPressed(key);
+    }
+    public static bool IsKeyDown(string keyString)
+    {
+        if (!KeyMap.TryGetValue(keyString, out KeyboardKey key))
+            throw new ArgumentException($"Key {keyString} not found.");
+        return Raylib.IsKeyPressed(key);
+    }
+}
