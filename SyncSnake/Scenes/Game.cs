87bd949de4b10174fe00ad5ea9b0612efa9dc2bc
FlorentTheb <florenthebault@gmail.com>
2025-05-28 15:42:24 +0200

    Add check for reducing or extending snake before moving (queue system)

--- SyncSnake/Scenes/Game.cs
+++ SyncSnake/Scenes/Game.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.Numerics;
 using GameTool;
 
 public class Game : IScene
@@ -27,11 +28,20 @@ public class Game : IScene
                 Grids[1].SetCell(row, column, 0);
             }
         }
+        Grids[0].SetCell(10, 10, 2);
+        Grids[1].SetCell(15, 7, 2);
+        Grids[0].SetCell(4, 15, 3);
+        Grids[0].SetCell(3, 5, 3);
+        Grids[1].SetCell(7, 7, 4);
     }
 
     public void Update()
     {
-
+        CheckInputs();
+        foreach (var snake in Snakes)
+        {
+            snake.Update();
+        }
     }
 
     public void Draw()
@@ -58,7 +68,13 @@ public class Game : IScene
         string color;
         if (value == 0)
             color = "DarkGray";
-        else color = "Green";
+        else if (value == 1)
+            color = "Green";
+        else if (value == 2)
+            color = "White";
+        else if (value == 3)
+            color = "Red";
+        else color = "Brown";
 
         Graphics.DrawSquare(coordinates.X, coordinates.Y, CellSize, "Black");
         Graphics.DrawSquare(coordinates.X, coordinates.Y, CellSize - 1, color);
@@ -70,5 +86,49 @@ public class Game : IScene
         int posX = offSet + columnIndex * CellSize;
         int posY = 210 + rowIndex * CellSize;
         return new Point(posX, posY);
+    }
+
+    private void CheckInputs()
+    {
+        List<string> nextMoveResults = new();
+        if (Keyboard.IsKeyPressed("Z"))
+        {
+            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(0, -1)));
+            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(0, -1)));
+        }
+        else if (Keyboard.IsKeyPressed("Q"))
+        {
+            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(-1, 0)));
+            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(-1, 0)));
+        }
+        else if (Keyboard.IsKeyPressed("S"))
+        {
+            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(0, 1)));
+            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(0, 1)));
+        }
+        else if (Keyboard.IsKeyPressed("D"))
+        {
+            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(1, 0)));
+            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(1, 0)));
+        }
+        if (nextMoveResults.Count > 0) // A key has been pressed
+        {
+            for (int snakeIndex = 0; snakeIndex < 2; snakeIndex++) // Check extend and reduce first
+            {
+                string result = nextMoveResults[snakeIndex];
+                if (result == "Apple") // Current snake can move, we check other snake
+                {
+                    Snakes[snakeIndex].HasToExtend = true;
+                    Snakes[1 - snakeIndex].HasToExtend = true;
+                }
+                else if (result == "Bomb")
+                {
+                    Snakes[1 - snakeIndex].Reduce();
+                }
+
+                if (result == "Empty" || result == "Apple" || result == "Bomb")
+                    Snakes[snakeIndex].HasToMove = true;
+            }
+        }
     }
 }
