2a7fddfb2601d1fa432d676fdd8fd8dfa6ea7d26
FlorentTheb <florenthebault@gmail.com>
2025-06-05 17:19:01 +0200

    Add image drawing to custom graphics lib

--- GameTool/Graphics.cs
+++ GameTool/Graphics.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Raylib_cs;
 
 namespace GameTool;
@@ -14,17 +15,37 @@ public static class Graphics
         { "LightGray", Color.LightGray },
         { "DarkGray", Color.DarkGray },
         { "Brown", Color.Brown },
+        { "Pink", Color.Pink },
     };
     public static void DrawRectangle(int posX, int posY, int width, int height, string color)
     {
-        if (!ColorMap.TryGetValue(color, out Color RaylibColor))
+        if (!ColorMap.TryGetValue(color, out Color raylibColor))
             throw new ArgumentException($"Color {color} not found. Must be given in Pascal Case");
         if (width <= 0 || height <= 0)
             throw new ArgumentOutOfRangeException($"Width and height should be positive. '{nameof(width)}' = {width} | '{nameof(height)}' = {height}");
-        Raylib.DrawRectangle(posX, posY, width, height, RaylibColor);
+        Raylib.DrawRectangle(posX, posY, width, height, raylibColor);
     }
     public static void DrawSquare(int posX, int posY, int sideLength, string color)
     {
         DrawRectangle(posX, posY, sideLength, sideLength, color);
+    }
+
+    public static void DrawImage(int posX, int posY, string color, Texture2D image, float angle)
+    {
+        if (!ColorMap.TryGetValue(color, out Color raylibColor))
+            throw new ArgumentException($"Color {color} not found. Must be given in Pascal Case");
+
+        Rectangle source = new Rectangle(0, 0, image.Width, image.Height);
+        Rectangle dest = new Rectangle(
+            posX + image.Width / 2f,
+            posY + image.Height / 2f,
+            image.Width,
+            image.Height
+        );
+        Vector2 origin = new Vector2(image.Width / 2f, image.Height / 2f);
+
+        Raylib.DrawTexturePro(image, source, dest, origin, angle, raylibColor);
+
+        // Raylib.UnloadTexture(image);
     }
 }
