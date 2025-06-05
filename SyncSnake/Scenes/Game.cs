4d737eacb9a4bae4c76e778f829194de1c66e04b
FlorentTheb <florenthebault@gmail.com>
2025-06-05 17:20:48 +0200

    Take in fact the next move of the other snake that can influence the new length of the current snake and remove the grid methods (now in the game grid)

--- SyncSnake/Scenes/Game.cs
+++ SyncSnake/Scenes/Game.cs
@@ -7,17 +7,17 @@ public class Game : IScene
     private int CurrentLevel;
     private const int CellNumber = 19;
     private const int CellSize = 30;
-    private List<Grid<int>> Grids = new List<Grid<int>>();
-    private List<Snake> Snakes = new List<Snake>();
+    private List<Grid> Grids = [];
+    private List<Snake> Snakes = [];
 
     public Game()
     {
         CurrentLevel = 1;
-        Grids.Add(new Grid<int>(CellNumber, CellNumber));
-        Grids.Add(new Grid<int>(CellNumber, CellNumber));
+        Grids.Add(new Grid(new Point(20, 210)));
+        Grids.Add(new Grid(new Point(610, 210)));
         InitCells();
-        Snakes.Add(new Snake(Grids[0]));
-        Snakes.Add(new Snake(Grids[1]));
+        Snakes.Add(new Snake(Grids[0], "Blue"));
+        Snakes.Add(new Snake(Grids[1], "Pink"));
     }
 
     private void InitCells()
@@ -36,124 +36,60 @@ public class Game : IScene
 
     public void Draw()
     {
-        DrawGrid();
+        DrawGrids();
+        DrawSnakes();
     }
 
-    private void DrawGrid()
+    public void DrawGrids()
     {
-        for (int index = 0; index < 2; index++)
+        foreach (var grid in Grids)
         {
-            for (int row = 0; row < Grids[0].Rows; row++)
-            {
-                for (int column = 0; column < Grids[0].Columns; column++)
-                {
-                    DrawCell(Grids[index].GetCell(row, column), GetGridCoordinates(index, row, column));
-                }
-            }
+            grid.Draw();
         }
     }
 
-    private void DrawCell(int value, Point coordinates)
+    public void DrawSnakes()
     {
-        string color;
-        if (value == 0)
-            color = "DarkGray";
-        else if (value == 1)
-            color = "Green";
-        else if (value == 2)
-            color = "White";
-        else if (value == 3)
-            color = "Red";
-        else color = "Brown";
-
-        Graphics.DrawSquare(coordinates.X, coordinates.Y, CellSize, "Black");
-        Graphics.DrawSquare(coordinates.X, coordinates.Y, CellSize - 1, color);
-    }
-
-    private Point GetGridCoordinates(int gridIndex, int rowIndex, int columnIndex)
-    {
-        int offSet = gridIndex == 0 ? 20 : 610;
-        int posX = offSet + columnIndex * CellSize;
-        int posY = 210 + rowIndex * CellSize;
-        return new Point(posX, posY);
-    }
-
-    private (int, int, int)? GetGridCell(int posX, int posY)
-    {
-        if (posY > 210 && posY <= 780)
+        foreach (var snake in Snakes)
         {
-            if (posX > 20 && posX <= 590)
-            {
-                int column = (posX - 20) / 30;
-                int row = (posY - 210) / 30;
-                return (0, row, column);
-            }
-            else if (posX > 610 && posX <= 1180)
-            {
-                int column = (posX - 610) / 30;
-                int row = (posY - 210) / 30;
-                return (1, row, column);
-            }
+            snake.Draw();
         }
-        return null;
     }
 
     private void CheckInputs()
     {
-        if (Mouse.IsLeftPressed())
-        {
-            (int, int, int)? gridIndexCellValuePair = GetGridCell(Mouse.GetX(), Mouse.GetY());
-            if (gridIndexCellValuePair != null)
-            {
-                int gridIndex = gridIndexCellValuePair.Value.Item1;
-                int row = gridIndexCellValuePair.Value.Item2;
-                int column = gridIndexCellValuePair.Value.Item3;
+        // if (Mouse.IsLeftPressed())
+        // {
+        //     (int, int, int)? gridIndexCellValuePair = GetGridCell(Mouse.GetX(), Mouse.GetY());
+        //     if (gridIndexCellValuePair != null)
+        //     {
+        //         int gridIndex = gridIndexCellValuePair.Value.Item1;
+        //         int row = gridIndexCellValuePair.Value.Item2;
+        //         int column = gridIndexCellValuePair.Value.Item3;
 
-                if (Grids[gridIndex].GetCell(row, column) == 0)
-                    Grids[gridIndex].SetCell(row, column, 2);
-                else if (Grids[gridIndex].GetCell(row, column) == 2)
-                    Grids[gridIndex].SetCell(row, column, 0);
-            }
+        //         if (Grids[gridIndex].GetCell(row, column) == 0)
+        //             Grids[gridIndex].SetCell(row, column, 2);
+        //         else if (Grids[gridIndex].GetCell(row, column) == 2)
+        //             Grids[gridIndex].SetCell(row, column, 0);
+        //     }
 
-        }
+        // }
         List<string> nextMoveResults = new();
+        string direction = "";
         if (Keyboard.IsKeyPressed("Z"))
-        {
-            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(0, -1)));
-            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(0, -1)));
-        }
+            direction = "Up";
         else if (Keyboard.IsKeyPressed("Q"))
-        {
-            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(-1, 0)));
-            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(-1, 0)));
-        }
+            direction = "Left";
         else if (Keyboard.IsKeyPressed("S"))
-        {
-            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(0, 1)));
-            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(0, 1)));
-        }
+            direction = "Down";
         else if (Keyboard.IsKeyPressed("D"))
-        {
-            nextMoveResults.Add(Snakes[0].CheckNextMove(new Vector2(1, 0)));
-            nextMoveResults.Add(Snakes[1].CheckNextMove(new Vector2(1, 0)));
-        }
-        if (nextMoveResults.Count > 0) // A key has been pressed
-        {
-            for (int snakeIndex = 0; snakeIndex < 2; snakeIndex++) // Check extend and reduce first
-            {
-                string result = nextMoveResults[snakeIndex];
-                if (result == "Apple") // Current snake can move, we check other snake
-                {
-                    Snakes[snakeIndex].HasToExtend = true;
-                    Snakes[1 - snakeIndex].HasToExtend = true;
-                }
-                else if (result == "Bomb")
-                {
-                    Snakes[1 - snakeIndex].Reduce();
-                }
+            direction = "Right";
 
-                if (result == "Empty" || result == "Apple" || result == "Bomb")
-                    Snakes[snakeIndex].HasToMove = true;
+        if (direction != "") // A key has been pressed
+        {
+            for (int snakeIndex = 0; snakeIndex < Snakes.Count; snakeIndex++) // Check extend and reduce first
+            {
+                Snakes[snakeIndex].UpdateNextLength(Snakes[1 - snakeIndex].CheckHeadNextMove(direction));
             }
         }
     }
@@ -173,17 +109,22 @@ public class Game : IScene
             {
                 if (row < 6 || row > Grids[0].Rows - 7 || column < 6 || column > Grids[0].Columns - 7)
                 {
-                    Grids[0].SetCell(row, column, 2);
-                    Grids[1].SetCell(row, column, 2);
+                    Grids[0].SetCell(row, column, CellType.Wall);
+                    Grids[1].SetCell(row, column, CellType.Wall);
                 }
                 else
                 {
-                    Grids[0].SetCell(row, column, 0);
-                    Grids[1].SetCell(row, column, 0);
+                    Grids[0].SetCell(row, column, CellType.Empty);
+                    Grids[1].SetCell(row, column, CellType.Empty);
                 }
 
-                Grids[0].SetCell(8, 10, 2);
-                Grids[1].SetCell(7, 10, 2);
+                Grids[0].SetCell(8, 10, CellType.Wall);
+                Grids[1].SetCell(7, 10, CellType.Wall);
+
+                Grids[0].SetCell(7, 7, CellType.Apple);
+                Grids[1].SetCell(11, 7, CellType.Apple);
+                Grids[1].SetCell(11, 10, CellType.Apple);
+                Grids[0].SetCell(11, 10, CellType.Bomb);
             }
         }
     }
