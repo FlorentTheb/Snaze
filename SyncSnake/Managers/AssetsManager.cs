6033a4bfc75c170abc5c45a21ed14ef320ca3c0a
FlorentTheb <florenthebault@gmail.com>
2025-06-05 19:55:09 +0200

    Add assets auomatically in output directory

--- SyncSnake/Managers/AssetsManager.cs
+++ SyncSnake/Managers/AssetsManager.cs
@@ -10,11 +10,11 @@ public static class AssetsManager
 
     private static readonly List<Texture2D> SnakeSprites = new()
     {
-        Raylib.LoadTexture("images/snake_head.png"),
-        Raylib.LoadTexture("images/snake_body_straight.png"),
-        Raylib.LoadTexture("images/snake_body_corner_right.png"),
-        Raylib.LoadTexture("images/snake_body_corner_left.png"),
-        Raylib.LoadTexture("images/snake_tail.png")
+        Raylib.LoadTexture("Assets/sprites/snake_head.png"),
+        Raylib.LoadTexture("Assets/sprites/snake_body_straight.png"),
+        Raylib.LoadTexture("Assets/sprites/snake_body_corner_right.png"),
+        Raylib.LoadTexture("Assets/sprites/snake_body_corner_left.png"),
+        Raylib.LoadTexture("Assets/sprites/snake_tail.png")
     };
 
     public static void DrawSprite(int spriteIndex, int posX, int posY, float spriteAngle, string color) =>
