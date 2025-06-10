19ce78b8c750ee3edb3d3e97e2a685f09684d83e
FlorentTheb <florenthebault@gmail.com>
2025-06-10 15:08:47 +0200

    Manager for keyboard inputs

---
+++ SyncSnake/Managers/InputsManager.cs
@@ -0,0 +1,36 @@
+using System;
+using GameTool;
+
+public static class InputsManager
+{
+    public static string direction { get; private set; } = "";
+    private static void Register(string newDirection)
+    {
+        direction = newDirection;
+    }
+
+    public static void CheckKeyboard()
+    {
+        string[] keys = { "Z", "Q", "S", "D" };
+
+        foreach (var key in keys)
+        {
+            if (Keyboard.IsKeyPressed(key))
+            {
+                Register(GetDirectionFromKeyboard(key));
+            }
+        }
+    }
+
+    public static string GetDirectionFromKeyboard(string key)
+    {
+        return key switch
+        {
+            "Z" => "Up",
+            "Q" => "Left",
+            "S" => "Down",
+            "D" => "Right",
+            _ => "_"
+        };
+    }
+}
