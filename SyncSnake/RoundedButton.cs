192f4228ce442cc79a87b0e60553a28b3d325c71
FlorentTheb <florenthebault@gmail.com>
2025-06-13 19:46:52 +0200

    Temporary neon rounded button

--- SyncSnake/RoundedButton.cs
+++ SyncSnake/RoundedButton.cs
@@ -7,34 +7,42 @@ public class RoundedButton : SimpleButton
     private readonly float Roundness = 1.0f;
     private readonly float Thickness = 8.0f;
     private readonly int Segments = 60;
-    private Color EdgeColor;
+    private Color EdgeColor1;
+    private Color EdgeColor2;
     public RoundedButton(string text, Rectangle bounds, Action action) : base(text, bounds, action)
     {
         DefaultColor = new Color
         {
-            R = 51,
-            G = 204,
-            B = 51,
+            R = 0,
+            G = 0,
+            B = 0,
             A = 255
         };
         HoverColor = new Color
         {
-            R = 112,
-            G = 219,
-            B = 112,
+            R = 255,
+            G = 255,
+            B = 255,
             A = 255
         };
         PressedColor = new Color
         {
-            R = 41,
-            G = 163,
-            B = 41,
+            R = 153,
+            G = 0,
+            B = 153,
             A = 255
         };
-        EdgeColor = new Color
+        EdgeColor1 = new Color
         {
-            R = 102,
+            R = 255,
             G = 255,
+            B = 255,
+            A = 150
+        };
+        EdgeColor2 = new Color
+        {
+            R = 255,
+            G = 102,
             B = 255,
             A = 255
         };
@@ -43,8 +51,13 @@ public class RoundedButton : SimpleButton
     public override void Draw()
     {
         Color currentColor = IsPressed ? PressedColor : IsHovered ? HoverColor : DefaultColor;
+        Color textColor = IsHovered ? new Color(255, 102, 255, 255) : Color.White;
+        Color neonOutsideColor = new Color(255, 102, 255, 150);
+        Raylib.DrawRectangleRoundedLinesEx(Bounds, Roundness, Segments, Thickness * 3 / 2, neonOutsideColor);
         Raylib.DrawRectangleRounded(Bounds, Roundness, Segments, currentColor);
-        Graphics.DrawText(Text, Bounds, "Black");
-        Raylib.DrawRectangleRoundedLinesEx(Bounds, Roundness, Segments, Thickness, EdgeColor);
+        Graphics.DrawText(Text, Bounds, textColor);
+        Raylib.DrawRectangleRoundedLinesEx(Bounds, Roundness, Segments, Thickness, EdgeColor2);
+        Rectangle neonInside = new Rectangle(Bounds.X - Thickness / 4, Bounds.Y - Thickness / 4, Bounds.Width + Thickness / 2, Bounds.Height + Thickness / 2);
+        Raylib.DrawRectangleRoundedLinesEx(neonInside, Roundness, Segments, Thickness / 2, EdgeColor1);
     }
 }
